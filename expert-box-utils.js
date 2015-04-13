ff61f4708dcfb3117f4fc0863628cb0f9ad95a51
Wynand <groendrakie@gmail.com>
2015-04-13 18:01:45 +0200

    added suicide code
    suicide code is useful when distributing free apps to wild testers

--- expert-box-utils.js
+++ expert-box-utils.js
@@ -81,39 +81,84 @@ module.service('ebUtils', ['$log', '_', 'string', function ($log, _, string) {
     return _.uniqueId(idText);
   };
 
-    //based on http://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-with-string-key
-    //return the property
-    this.get = function(obj, str) {
-        var functionName = moduleName+'.get';
+  //used for auto destruct of apps distributed for testing in the wild
+  //kills javascript on the page, aka suicide code
+  //credits: http://stackoverflow.com/questions/550574/how-to-terminate-the-script-in-javascript
+  this.exit( status ) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir (http://brettz9.blogspot.com)
+    // +      input by: Paul
+    // +   bugfixed by: Hyam Singer (http://www.impact-computing.com/)
+    // +   improved by: Philip Peterson
+    // +   bugfixed by: Brett Zamir (http://brettz9.blogspot.com)
+    // %        note 1: Should be considered expirimental. Please comment on this function.
+    // *     example 1: exit();
+    // *     returns 1: null
 
-        if (angular.isDefined(obj) && angular.isDefined(str)) {
-            //convert indexes to properties
-            str = str.replace(/\[(\w+)\]/g, '.$1');  
-            //remove leading dot
-            str = str.replace(/^\./, '');
-            var props = str.split('.');
-            //while there are more properties
-            while (props.length) {
-                //shift a property
-                var prop = props.shift();
-                //if object has defined property 
-                if (_.has(obj,prop) && angular.isDefined(obj[prop])) {
-                    //set object to object.property
-                    obj = obj[prop];
-                } else {
-                    //cannot find prop
-                    //$log.debug(functionName+': cannot find defined prop '+prop);
-                    return undefined;
-                }
-            }
-            return obj;
-        }
-        else {
-            //input validation failed
-            //$log.debug(functionName+': input validation failed');
-            return undefined;
-        }
-    };
+    var i;
+
+    if (typeof status === 'string') {
+        alert(status);
+    }
+
+    window.addEventListener('error', function (e) {e.preventDefault();e.stopPropagation();}, false);
+
+    var handlers = [
+        'copy', 'cut', 'paste',
+        'beforeunload', 'blur', 'change', 'click', 'contextmenu', 'dblclick', 'focus', 'keydown', 'keypress', 'keyup', 'mousedown', 'mousemove', 'mouseout', 'mouseover', 'mouseup', 'resize', 'scroll',
+        'DOMNodeInserted', 'DOMNodeRemoved', 'DOMNodeRemovedFromDocument', 'DOMNodeInsertedIntoDocument', 'DOMAttrModified', 'DOMCharacterDataModified', 'DOMElementNameChanged', 'DOMAttributeNameChanged', 'DOMActivate', 'DOMFocusIn', 'DOMFocusOut', 'online', 'offline', 'textInput',
+        'abort', 'close', 'dragdrop', 'load', 'paint', 'reset', 'select', 'submit', 'unload'
+    ];
+
+    function stopPropagation (e) {
+        e.stopPropagation();
+        // e.preventDefault(); // Stop for the form controls, etc., too?
+    }
+    for (i=0; i < handlers.length; i++) {
+        window.addEventListener(handlers[i], function (e) {stopPropagation(e);}, true);
+    }
+
+    if (window.stop) {
+        window.stop();
+    }
+
+    throw '';
+  };
+}
+
+  //based on http://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-with-string-key
+  //return the property
+  this.get = function(obj, str) {
+      var functionName = moduleName+'.get';
+
+      if (angular.isDefined(obj) && angular.isDefined(str)) {
+          //convert indexes to properties
+          str = str.replace(/\[(\w+)\]/g, '.$1');  
+          //remove leading dot
+          str = str.replace(/^\./, '');
+          var props = str.split('.');
+          //while there are more properties
+          while (props.length) {
+              //shift a property
+              var prop = props.shift();
+              //if object has defined property 
+              if (_.has(obj,prop) && angular.isDefined(obj[prop])) {
+                  //set object to object.property
+                  obj = obj[prop];
+              } else {
+                  //cannot find prop
+                  //$log.debug(functionName+': cannot find defined prop '+prop);
+                  return undefined;
+              }
+          }
+          return obj;
+      }
+      else {
+          //input validation failed
+          //$log.debug(functionName+': input validation failed');
+          return undefined;
+      }
+  };
 
     /*
   The MIT License (MIT)
