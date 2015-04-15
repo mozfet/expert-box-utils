178e84114f82abd6f26144cdacf3649c702c7f9d
Wynand <groendrakie@gmail.com>
2015-04-15 12:58:57 +0200

    added manual compile function for recursive directives

--- expert-box-utils.js
+++ expert-box-utils.js
@@ -81,6 +81,42 @@ module.service('ebUtils', ['$log', '_', 'string', function ($log, _, string) {
     return _.uniqueId(idText);
   };
 
+  //manually compile an element, used to break the recusion loop for directives
+  //credits : Mark Lagendijk on stackoverflow, http://stackoverflow.com/questions/14430655/recursion-in-angular-directives
+  //example : http://plnkr.co/edit/JAIyolmqPqO9KsynSiZp?p=preview
+  this.compile = function (element, link) {
+    // Normalize the link parameter
+    if(angular.isFunction(link)){
+      link = { post: link };
+    }
+
+    // Break the recursion loop by removing the contents
+    var contents = element.contents().remove();
+    var compiledContents;
+    return {
+      pre: (link && link.pre) ? link.pre : null,
+      /**
+        * Compiles and re-adds the contents
+        */
+        post: function(scope, element){
+          // Compile the contents
+          if(!compiledContents){
+            compiledContents = $compile(contents);
+          }
+   
+          // Re-add the compiled contents to the element
+          compiledContents(scope, function(clone){
+            element.append(clone);
+          });
+
+          // Call the post-linking function, if any
+          if(link && link.post){
+            link.post.apply(null, arguments);
+          }
+        }
+    };
+  };
+
   //used for auto destruct of apps distributed for testing in the wild
   //kills javascript on the page, aka suicide code
   //credits: http://stackoverflow.com/questions/550574/how-to-terminate-the-script-in-javascript
